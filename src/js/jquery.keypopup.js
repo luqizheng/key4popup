039d64bf30581ad20648f492145d23d7183f4268
luqizheng <luqizheng@126.com>
2015-12-21 20:36:31 +0800

    基本完成。ie测试

--- src/js/jquery.keypopup.js
+++ src/js/jquery.keypopup.js
@@ -1,29 +1,43 @@
 (function ($) {
     $.fn.keypopup = function (opt) {
-
+        var defaultOpt = {
+            onMatch: false, // match pop up condition
+            onMiss: false, // missmatch ,
+            onFocus: false, //for select start.
+            onDefault: false, //use press to select the first one. it should  return default one.
+            isShow: false,
+            atId: "at", // get the text position by this id,
+            _curPos: {
+                start: 0,
+                end: 0,
+                tag: false,
+            },
+            _state: 0 // 0 nothing , 1 onMatch.
+        }
 
         var options = $.extend({}, defaultOpt, opt),
             $this = $(this);
         if (typeof opt == "string") {
+            options = $(this).data("_keypopup");
             switch (opt) {
                 case "set":
-                    options = $(this).data("_keypopup");
-                    setText.call(this, options, arguments[1]);
+                    pub.setText.call(this, options, arguments[1]);
                     break;
+                case "focus":
+                    pub.focus.call(this, options);
             }
         }
         else {
 
-            $this.click(function () {
-                resetDiv.call(this);
-            })
+            $this
                 .data("_keypopup", options)
                 .mouseup(function (e) {
+                    resetDiv.call(this);//reset the layout position.
                     if (renderToLayer.call(this, options)) {
-                        fireOnMatch.call(this, options);
+                        fire.match.call(this, options);
                     }
                     else {
-                        options.onMiss();
+                        fire.miss.call(this, options);
                     }
                     e.stopPropagation();
                     e.preventDefault();
@@ -31,55 +45,74 @@
                 })
                 .keyup(function (e) {
                     console.debug("keyup " + e.which);
-                    if(e.which==27){
-                        options.onMiss.call(this);
+                    var useAtChar = (e.which === 50 && e.shiftKey),
+                        bMatchFocus = false;
+                    if (e.which == 27) {
+                        fire.miss.call(this, options);//ESC 
+                        return;
                     }
-                    var useAtChar = (e.which === 50 && e.shiftKey);
-                    if (e.which == 38 || e.which == 39 || e.which == 40 || e.which == 37 || e.which == 8 || useAtChar) {
-                        if (renderToLayer.call(this, options, useAtChar)) {
-                            fireOnMatch.call(this, options);
+                    if (options._state == 1) {
+                        if (e.which == 40) {
+                            console.log("on focuse the panel.")
+                            fire.focus.call(this, options);
+                            bMatchFocus = true;
                         }
+                        if (e.which == 13) {
+                            fire.default.call(this, options);
+                            bMatchFocus = true;
+                        }
+                    }
+                    if (!bMatchFocus && (e.which == 38 || e.which == 39 || e.which == 40 || e.which == 37 || e.which == 8 || e.which == 229 || useAtChar)) {
+                        var method = renderToLayer.call(this, options, useAtChar) ? "match" : "miss";
+                        fire[method].call(this, options);
                     }
                     e.stopPropagation();
                     e.preventDefault();
                     return false;
-                }).keydown(function (e) {
-                    console.debug("keydown " + e.which);
-                    //when the popup panel is showing, it should be focus on the textarea,
-                    //user use entier or down key boadrd, it should be insert to the text.
-                    if (e.which == 40 && options._state == 1) {  
-                        console.log("on focuse the panel.")
-                        e.stopPropagation();
-                        e.preventDefault();
-                        fireOnFocus.call(this, options);
-                        return false;
-                    }
-
                 });
             resetDiv.call(this);
         }
         return $this;
     }
 
-    function setText(options, strName) {
-        var start = $(this).val().substr(0, options._curPos.start);
-        var end = $(this).val().substr(options._curPos.end);
-        var result = start + "@" + strName + end;
-        $(this).val(result);
-        setCursor.call(this, options, strName.length);
-        options._state = 0;
-        options.onMiss.call(this);
+    var fire = {
+        default: function (options) {
+            var d = options.onDefault.call(this, options);
+            if (d) {
+                pub.setText.call(this, options, d);
+            }
+        },
+        match: function (options) {
+            var $tag = $("#" + options.atId);
+            var offset = $tag.offset();
+            offset.top += $tag.height();
+            options._state = 1;
+            options.onMatch.call(this, offset);
+        },
+        miss: function (options) {
+            options.onMiss.call(this)
+            options._state = 0;
+        },
+        focus: function (options) {
+            options.onFocus.call(this);
+            options._state = 0;
+        }
     }
 
-    function fireOnMatch(options) {
-        var offset = $("#" + options.atId).offset();
-        offset.top += $("#" + options.atId).height();
-        options._state = 1;
-        options.onMatch.call(this, offset);
-    }
-    function fireOnFocus(options) {
-        options._state = 0;
-        options.onFocus.call(this);
+    var pub = {
+        setText: function (options, strName) {
+            var start = $(this).val().substr(0, options._curPos.start);
+            var end = $(this).val().substr(options._curPos.end);
+            var result = start + "@" + strName + end;
+            $(this).val(result);
+            setCursor.call(this, options, strName.length);
+            options._state = 0;
+            options.onMiss.call(this);
+        },
+        focus: function (options) {
+            setCursor.call(this, options);
+        }
+
     }
 
     function getSelection(options) {
@@ -93,22 +126,32 @@
             dup_range.moveToElementText(textarea);
             dup_range.setEndPoint('EndToEnd', range);
             content = dup_range.text;
-            alert("IE no test.")
         }
         return content;
     }
 
     function setCursor(options, wordLength) {
-        var $self = $(this).focus();
-        if (window.getSelection) {
-            var newLength = wordLength + options._curPos.start + 1;
-            console.debug("end curPos:" + newLength)
-            $self[0].selectionEnd = newLength;
-            $self[0].selectionStart = newLength;
+        var self=this, $self = $(self).focus();
+        if (!wordLength) {
+            wordLength = options._curPos.tag.length;
         }
+        var newLength = wordLength + options._curPos.start + 1;
+        console.debug("end curPos:" + newLength)
+        if (window.getSelection) {
+            self.selectionEnd = newLength;
+            self.selectionStart = newLength;
+        }
+        else if (self.createTextRange) {
+            var range = self.createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', newLength);
+            range.moveStart('character', newLength);
+        }
+
     }
 
-    function renderToLayer(options, byAtKeyUp) {
+
+    function renderToLayer(options) {
         //render the text which mouse focus to the layout behinde the textarea.
         var textarea = this,
             content = getSelection.call(this, options),
@@ -125,26 +168,24 @@
                 halfName = halfName[0].substring(0, halfName[0].length - 1)
             }
             content = content.substring(0, content.length - matches[0].length);
-            
+
             completeTag = matches[0] + halfName; //without @ char.
             
         }
-        else if (byAtKeyUp) // fire by @ keyup, it may no match anything, when cursor in the end of textbox.
-        {
-            content=content.substring(0,content.length-1);
+        else if (content.lastIndexOf("@") == Math.abs(content.length - 1)) {
+            isMatch = true;
+            content = content.substring(0, content.length - 1);
             //content hasn't any change. 
             completeTag = "@";
         }
-        console.debug("content without name:"+content+" complete name:"+completeTag)
+        console.debug("content without name:" + content + " complete name:" + completeTag)
         options._curPos = {
             start: content.length,
-            end: content.length + completeTag.length
+            end: content.length + completeTag.length,
+            tag: completeTag
         }
-        options._target.html(content + "<span id='" + options.atId + "'>" + completeTag + "</span>");
-
-
-
-        return isMatch || byAtKeyUp;
+        options._target.html(content.replace(/[\r\n]/g, "<br>") + "<span id='" + options.atId + "'>" + completeTag + "</span>");
+        return isMatch;
     }
 
     function resetDiv() {
@@ -156,7 +197,7 @@
                 width: $this.innerWidth()
             }
         if (!options._target) {
-            options._target = $('<div style="position:absolute;width;background-color:red;z-index:-99999;overflow:hidden"></div>').appendTo("body");
+            options._target = $('<div style="position:absolute;width;z-index:-99999;overflow:hidden;visiblity:hidden"></div>').appendTo("body");
         }
         options._target
             .css("width", $this[0].clientWidth)
@@ -165,18 +206,7 @@
             .css("top", offset.top);
     }
 
-    var defaultOpt = {
-        onMatch: false, // match pop up condition
-        onMiss: false, // missmatch ,
-        onFocus: false, //for select start. 
-        
-        atId: "at",
-        _curPos: {
-            start: 0,
-            end: 0,
-        },
-        _state: 0 // 0 nothing , 1 onMatch.
-    }
+
 
     function _shoJson(obj) {
         alert(JSON.stringify(obj))
