6cf0abbd531a8c9dd679298fa93d936791276d4a
luqizheng <luqizheng@126.com>
2015-12-24 15:32:00 +0800

    尝试添加banner

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-"use strict";
+﻿"use strict";
 
 var gulp = require("gulp"),
     rimraf = require("rimraf"),
@@ -6,26 +6,29 @@ var gulp = require("gulp"),
     cssmin = require("gulp-cssmin"),
     uglify = require("gulp-uglify"),
     gutil = require("gulp-util"),
-    rename = require("gulp-rename");
+    rename = require("gulp-rename"),
+    license=require("uglify-save-license");
 
 var version = "1.0" //当前版本号
 var now = new Date();  //构建日期
 var date = now.getFullYear() + "." + (now.getMonth() + 1) + "." + now.getDate()
 
 var paths = {
-    src: "./js/*.js",
-    dist: "dist"
+    src: "./src/*.js",
+    dist: "dist",
+    name:"jquery.keypopup.js"
 }
 
 gulp.task("default", function (cb) {    
-    return gulp.src(paths.src)        
-        .pipe(uglify())
+    return gulp.src(paths.src)
+        .pipe(concat(paths.name))               
+        .pipe(uglify({preserveComments:license}))
         .pipe(rename({
             suffix: '-'+version+'.min'
-        }))
+        }))        
         .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch(paths.src, ['default']);   //监听 RAW/js 下的全部.js文件，若有改动则执行名为'minjs'任务
+    gulp.watch(paths.src, ['default']);
 });
---
+++ src/0summary.js
@@ -0,0 +1,8 @@
+/*!
+ jquery keypopup @version
+ Project under the MIT.
+ https://github.com/luqizheng/key4popup
+ 
+ Released under the MIT license
+ //
+!*/
